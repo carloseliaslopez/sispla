dc1e15e669c8435b5d179411f7630b2d82f3013c
carloseliaslopez <carloseliaslopez2015@gmail.com>
2023-11-23 16:49:02 -0600

    reparando errores de conexion

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $rol = $_SESSION ['idRol'];
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <link rel="shortcut icon" href="./dist/images/VERSATEC.svg">
+    <link href="./dist/images/VERSATEC.svg" rel="icon" >
 
     <title>Pagina Principal</title>
     <link href="css/styles.css" rel="stylesheet" />
