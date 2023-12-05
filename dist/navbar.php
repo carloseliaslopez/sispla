b188c97f7d91dc0efe630799327faf8a22cf3be9
carloseliaslopez <carloseliaslopez2015@gmail.com>
2023-12-05 16:20:43 -0600

    Integracion de monitoreo

--- dist/navbar.php
+++ dist/navbar.php
@@ -1,10 +1,10 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark" style="background-color:#2B3990;">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark" style="background-color:#2B3990;">
             <a class="navbar-brand" href="index.php">SISPLA</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             
             <!-- Navbar-->
-            <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
+            <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0" >
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <?php echo $nombre; ?> <i class="fas fa-user fa-fw"></i></a>
@@ -15,7 +15,7 @@
                     </div>
                 </li>
             </ul>
-        </nav>
+    </nav>
 
 
         
