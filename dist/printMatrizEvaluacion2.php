fa7fadb7ccc82e88aa0a0c4e60b8906b13d22c75
carloseliaslopez <carloseliaslopez2015@gmail.com>
2023-12-11 16:42:53 -0600

    ajustando impresiones

--- dist/printMatrizEvaluacion2.php
+++ dist/printMatrizEvaluacion2.php
@@ -72,16 +72,15 @@ $conexion = new mysqli('localhost','admin','adminCump123.','sispla');
                     <h4 id="h1Informe" name= "h1Informe"  >Informe de Debida Diligencia</h4>
                     <hr>
                     <div class="justify-content-md-center" >
-                        <div class="form-row">
+                        <div class="form-row-inline">
                             <div class="form-group col-sm-6">
-
                                 <label  for="razonSocial_ME" ><b> Nombre del Cliente</b></label>
-                                <input type="text" class="form-control form-control-sm-6" id="razonSocial_ME" name="razonSocial_ME" 
+                                <input type="text" class="form-control form-control-sm" id="razonSocial_ME" name="razonSocial_ME" 
                                 placeholder="Razón Social o Nombre Completo del Cliente"  value="<?php echo $empEdit->__GET('cliente') ?>" disabled>
                             </div>  
                             <div class="form-group col-sm-3">
                                 <label for="tipoCliente_ME" ><b>Tipo de Cliente</b></label>
-                                <input type="text" class="form-control form-control-sm-3" id="tipoCliente_ME" name="tipoCliente_ME" 
+                                <input type="text" class="form-control form-control-sm" id="tipoCliente_ME" name="tipoCliente_ME" 
                                     placeholder="Tipo de Cliente" value="<?php echo $empEdit->__GET('tipoCliente') ?>" disabled>
                             </div>
                         </div>    
