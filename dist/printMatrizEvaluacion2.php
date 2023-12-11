7fd7fc9c8730326a617fe316e55322e90ed1480e
carloseliaslopez <carloseliaslopez2015@gmail.com>
2023-12-11 15:44:23 -0600

    ajustando impresiones

--- dist/printMatrizEvaluacion2.php
+++ dist/printMatrizEvaluacion2.php
@@ -48,7 +48,7 @@ $conexion = new mysqli('localhost','admin','adminCump123.','sispla');
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1 , shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Informe IDD</title>
