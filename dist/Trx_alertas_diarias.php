0c242e66d7fc5fc3598b8fdf0b41b74d9661de1e
carloseliaslopez <carloseliaslopez2015@gmail.com>
2023-12-28 14:27:31 -0600

    Actualizando logos

--- dist/Trx_alertas_diarias.php
+++ dist/Trx_alertas_diarias.php
@@ -250,7 +250,9 @@ if(isset($varMsjNewEmp))
         <script>
             $(document).ready(function ()
             {
+                
                 ////// APLICAMOS FORMATO Y BOTONES A LA TABLA //// INICIAMOS EL PLUGIN DATATABLE
+                /*
                 $('#busquedaInterna').DataTable({
                     dom: 'Bfrtip',
                     buttons: [
@@ -260,6 +262,7 @@ if(isset($varMsjNewEmp))
                     ]
 
                 });
+                */
 
                 /////////// VARIABLES DE CONTROL MSJ ///////////
                 var newEmp = 0;
@@ -293,12 +296,81 @@ if(isset($varMsjNewEmp))
             });
         </script>
 
-    <script>
+        <script>
           // Tratatando de realizar el cambio 
           function revisar($id)
             {
                 window.open("Trx_cierre_alerta.php?trx="+$id, "self");
             }
         </script>
+
+        <script>
+            $(document).ready(function () {
+                // Setup - add a text input to each footer cell
+                $('#busquedaInterna thead tr')
+                    .clone(true)
+                    .addClass('filters')
+                    .appendTo('#busquedaInterna thead');
+                    
+            
+                var table = $('#busquedaInterna').DataTable({
+                    
+                    orderCellsTop: true,
+                    fixedHeader: true,
+                    initComplete: function () {
+                        var api = this.api();
+            
+                        // For each column
+                        api
+                            .columns()
+                            .eq(0)
+                            .each(function (colIdx) {
+                                // Set the header cell to contain the input element
+                                var cell = $('.filters th').eq(
+                                    $(api.column(colIdx).header()).index()
+                                );
+                                var title = $(cell).text();
+                                $(cell).html('<input type="text" placeholder="' + title + '" />');
+            
+                                // On every keypress in this input
+                                $(
+                                    'input',
+                                    $('.filters th').eq($(api.column(colIdx).header()).index())
+                                )
+                                    .off('keyup change')
+                                    .on('change', function (e) {
+                                        // Get the search value
+                                        $(this).attr('title', $(this).val());
+                                        var regexr = '({search})'; //$(this).parents('th').find('select').val();
+            
+                                        var cursorPosition = this.selectionStart;
+                                        // Search the column for that value
+                                        api
+                                            .column(colIdx)
+                                            .search(
+                                                this.value != ''
+                                                    ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                                    : '',
+                                                this.value != '',
+                                                this.value == ''
+                                            )
+                                            .draw();
+                                    })
+                                    .on('keyup', function (e) {
+                                        e.stopPropagation();
+            
+                                        $(this).trigger('change');
+                                        $(this)
+                                            .focus()[0]
+                                            .setSelectionRange(cursorPosition, cursorPosition);
+                                    });
+                            });
+                    },
+                });
+            });
+        </script>
+
+
+
     </body>
 </html>
